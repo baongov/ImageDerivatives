74e7ef47d9982985195667c0e5b876305a9673a8
ngovietbao <ngothivietbao@gmail.com>
2016-10-28 22:22:10 +0700

    edit some errors

--- ImageProccessing.cpp
+++ ImageProccessing.cpp
@@ -9,9 +9,9 @@
 Mat ImageProccessing::CreateParodayMartrix(int height, int weight)
 {
   Mat parody = Mat::zeros(rows + 2*height - 2, cols + 2*weight - 2 , CV_64F);
-  for (int i = 0; i < rows_i; i++)
-    for (int j = 0; j < cols_i; j++)
-      parody.at<double>(i + rows_h - 1,j + cols_h - 1) = input.at<double>(i,j);
+  for (int i = 0; i < rows; i++)
+    for (int j = 0; j < cols; j++)
+      parody.at<double>(i + height - 1,j + weight - 1) = input.at<double>(i,j);
 
   return parody;
 }
